80e9b5a2d339243631cb54b5b15397ad53c1e924
Rachmawaty <rachmawatyy@gmail.com>
2016-07-14 00:22:01 +0100

    make each visualisation and category modular

--- app.js
+++ app.js
@@ -2,27 +2,15 @@
 var express = require('express');
 var app = express();
 
-// var config = require('./config.js')(app, express);
-
 /*** Path and Filesystem ***/
 app.path = require('path');
 app.fs = require('fs');
 
-// app.use(express.compress());
 app.set('views', app.path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(express.static(app.path.join(__dirname, 'public')));
-// app.use(bodyParser.json());
 
-// jsonld
-// var jsonld = require('jsonld');
-
-//D3.js
-// var d3 = require('d3');
-// var jsdom = require('jsdom');
-// var document = jsdom.jsdom();
-// var svg = d3.select(document.body).append("svg");
+/*** Pug ***/
+app.set('view engine', 'pug');
 
 var next = function() {
     app.listen(2525);
@@ -31,23 +19,6 @@ var next = function() {
 };
 
 next();
-
-//homepage
-// app.get('/', function(req, res){
-// 	res.sendFile('index.html', {root: app.path.join(__dirname, './views')});
-// });
-
-app.get('/datasets', function(req, res){
-	res.sendFile('datasets.html', {root: app.path.join(__dirname, './views')});
-});
-
-app.get('/visualisation', function(req, res){
-	res.sendFile('visualisation.html', {root: app.path.join(__dirname, './views')});
-});
-
-app.get('/about', function(req, res){
-	res.sendFile('about.html', {root: app.path.join(__dirname, './views')});
-});
 
 //port --> localhost:2525
 // app.listen(2525, function(){
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,22 @@ module.exports = function(app){
 	});
 
 	app.get('/', function(req, res){
-		res.render('main.pug', res.status(200));
+		res.render('index.pug', { active:"index" });
+	});
+
+	app.get('/visualisation', function(req, res){
+		res.render('visualisation.pug', { active:"visualisation" });
+	});
+
+	app.get('/datasets', function(req, res){
+		res.render('datasets.pug', { active:"datasets" });
+	});
+
+	app.get('/about', function(req, res){
+		res.render('about.pug', { active:"about" });
+	});
+
+	app.get('/table', function(req, res){
+		res.render('table.pug');
 	});
 }
