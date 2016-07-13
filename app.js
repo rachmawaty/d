99c8c50a2e5d02f518b15c99740a98492f421386
Rachmawaty <rachmawatyy@gmail.com>
2016-07-13 19:21:46 +0100

    layout template -> main.pug

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
-//Express Framework
+/*** Express Framework ***/
 var express = require('express');
 var app = express();
 
-//Path and Filesystem
-var path = require('path');
-var fs = require('fs');
+// var config = require('./config.js')(app, express);
 
-app.use(express.static(path.join(__dirname, 'public')));
+/*** Path and Filesystem ***/
+app.path = require('path');
+app.fs = require('fs');
 
-//jsonld
-var jsonld = require('jsonld');
+// app.use(express.compress());
+app.set('views', app.path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(app.path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
+
+// jsonld
+// var jsonld = require('jsonld');
 
 //D3.js
-var d3 = require('d3');
+// var d3 = require('d3');
 // var jsdom = require('jsdom');
 // var document = jsdom.jsdom();
 // var svg = d3.select(document.body).append("svg");
 
+var next = function() {
+    app.listen(2525);
+    console.log("Express server listening on port 2525");
+    require('./routes/routes')(app);
+};
+
+next();
+
 //homepage
-app.get('/', function(req, res){
-	res.sendFile('index.html', {root: path.join(__dirname, './views')});
-});
+// app.get('/', function(req, res){
+// 	res.sendFile('index.html', {root: app.path.join(__dirname, './views')});
+// });
 
 app.get('/datasets', function(req, res){
-	res.sendFile('datasets.html', {root: path.join(__dirname, './views')});
+	res.sendFile('datasets.html', {root: app.path.join(__dirname, './views')});
 });
 
 app.get('/visualisation', function(req, res){
-	res.sendFile('visualisation.html', {root: path.join(__dirname, './views')});
+	res.sendFile('visualisation.html', {root: app.path.join(__dirname, './views')});
 });
 
 app.get('/about', function(req, res){
-	res.sendFile('about.html', {root: path.join(__dirname, './views')});
+	res.sendFile('about.html', {root: app.path.join(__dirname, './views')});
 });
 
 //port --> localhost:2525
-app.listen(2525, function(){
-	console.log('Listening at port 2525');
-});
+// app.listen(2525, function(){
+// 	console.log('Listening at port 2525');
+// });
